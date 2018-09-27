94f242d031227cf183c9febce3969227cd2a04b1
Vaishali Agola <vaishali.agola@rtcamp.com>
2018-09-27 17:10:00 +0530

    public url for mu-plugins added

--- .circleci/setup_env.sh
+++ .circleci/setup_env.sh
@@ -2,7 +2,7 @@
 
 setup_environment() {
   echo "Setting up env variables"
-  mu_plugins_url="https://github.com/Automattic/vip-go-mu-plugins.git"
+  mu_plugins_url="https://github.com/Automattic/vip-mu-plugins-public"
   temp_directory=$(mktemp -d 2>/dev/null || mktemp -d -t 'build')
   project_root="$( git rev-parse --show-toplevel )"
   build_root="$temp_directory"
