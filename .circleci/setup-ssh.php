4862eab21d9cca86fc2321f4c198184190eb542d
Vaishali Agola <vaishali.agola@rtcamp.com>
2018-09-28 13:08:25 +0530

    Removed server.json content to deploy

--- .circleci/setup-ssh.php
+++ .circleci/setup-ssh.php
@@ -22,8 +22,8 @@ $current_branch = getenv( 'CIRCLE_BRANCH' );
 if ( json_last_error() === JSON_ERROR_NONE ) {
 
 	// Send key value if key is set in server details variable
-	if ( isset( $server_keys[ $current_branch ] ) && ! empty( $server_keys[ $current_branch ]->key ) ) {
-		return $server_keys[ $current_branch ]->key;
+	if ( isset( $server_keys[ $current_branch ] ) && ! empty( $server_keys[ $current_branch ]['key'] ) ) {
+		return $server_keys[ $current_branch ]['key'];
 	} else {
 		return $default_key;
 	}
