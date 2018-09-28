61fcc42cbf4a6bc99591d76956998cefe49d4542
Vaishali Agola <vaishali.agola@rtcamp.com>
2018-09-28 12:57:08 +0530

    print server variable to check value

--- .circleci/setup-ssh.php
+++ .circleci/setup-ssh.php
@@ -2,7 +2,7 @@
 
 if ( file_exists( 'server.json' ) ) {
 
-	$file_content = json_decode( file_get_contents ("server.json") );
+	$file_content = json_decode( file_get_contents ("server.json"), true );
 
 }
 
@@ -15,7 +15,7 @@ if ( ! empty( $file_content ) && json_last_error() === JSON_ERROR_NONE ) {
 	$server_keys = json_decode( getenv( 'SERVER_DETAILS' ), true );
 
 }
-
+sprintf( print_r( $server_keys, true ) );
 $default_key    = getenv( 'SSH_FINGERPRINT' );
 $current_branch = getenv( 'CIRCLE_BRANCH' );
 
